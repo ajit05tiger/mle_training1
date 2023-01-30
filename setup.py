4e5a164e8a397b2512b3dff150c9ad36155508ec
Ajit <ajit.mishra@tigeranalytics.com>
2023-01-31 03:07:48 +0530

    final package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="py_packages",
+    version="0.1",
+    description="This is code for predicting house price",
+    author="ajit05tiger",
+    install_requires=[],
+)
